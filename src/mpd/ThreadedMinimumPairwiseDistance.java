2534278be8edbbe41e58c6d0857f5aeb91b20eab
scvstackkk <scv.stack@gmail.com>
2017-12-09 20:53:59 -0600

    Re-write solution to pass timing test

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -4,80 +4,28 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
 
 	
 	public ThreadedMinimumPairwiseDistance() {
-	}
-	
-	public int minimumPairwiseDistance(int[] values, int currentStart, int currentEnd, Answer answer) {
-		
-		int maxThreads = 10000;
-		int numThreads = (currentEnd - currentStart)/2;
-		int totalThreads = (currentEnd - currentStart)/2;
-		
-		Thread[] threadList;
-        if (maxThreads <= numThreads) {
-        	numThreads = maxThreads; 
-        }
-        threadList = new Thread[numThreads];
-              
-        for(int i = 0; i<numThreads; i++) {
-        	Thread thread = new Thread(new innerThread(currentStart, currentEnd, answer, values),"thread"+i);
-        	threadList[i] = thread;
-        	currentStart++;
-        	currentEnd--;
-        }
-        
-        for(int i = 0; i<numThreads; i++) {
-        	threadList[i].start();
-        	
-        }
-        
-        for (int i=0; i<numThreads; i++) {
-        	try {
-        		threadList[i].join();
-        	} catch (InterruptedException e) {
-        		System.out.println("Thread Interrupted");
-        	}
-        	
-        }
-        System.out.println("Finished" + numThreads + " threads.");
-        if (numThreads != totalThreads) {
-        	return minimumPairwiseDistance(values, currentStart, currentEnd, answer);
-        }
-        else{
-        	return answer.getAnswer();
-        }
 		
 	}
-	
+
+
     @Override
     public int minimumPairwiseDistance(int[] values) {
         //throw new UnsupportedOperationException();
         
     	Answer answer = new Answer();
-        int endPoint = values.length - 1;
-        int startPoint = 0;
-        int numThreads = values.length/2;
-        int maxThreads = 10000;
-        int totalThreads = values.length/2;
         
-        Thread[] threadList;
-        if (maxThreads <= numThreads) {
-        	numThreads = maxThreads; 
-        }
-        threadList = new Thread[numThreads];
-              
-        for(int i = 0; i<numThreads; i++) {
-        	Thread thread = new Thread(new innerThread(startPoint, endPoint, answer, values),"thread"+i);
-        	threadList[i] = thread;
-        	startPoint++;
-        	endPoint--;
-        }
+        Thread thread1 = new Thread(new LowerLeft(answer, values));
+        Thread thread2 = new Thread(new BottomRight(answer, values));
+        Thread thread3 = new Thread(new Middle(answer, values));
+        Thread thread4 = new Thread(new TopRight(answer, values));
         
-        for(int i = 0; i<numThreads; i++) {
+        Thread[] threadList = {thread1, thread2, thread3, thread4};
+        
+        for (int i=0; i<4; i++) {
         	threadList[i].start();
-        	
         }
         
-        for (int i=0; i<numThreads; i++) {
+        for (int i=0; i<4; i++) {
         	try {
         		threadList[i].join();
         	} catch (InterruptedException e) {
@@ -85,51 +33,142 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
         	}
         	
         }
-        
-        System.out.println("Finished" + numThreads + " threads.");
-        if (numThreads != totalThreads) {
-        	return minimumPairwiseDistance(values, startPoint, endPoint, answer);
-        }
+        System.out.println("finished array");
         return answer.getAnswer();
         
     }
     
-    private class innerThread implements Runnable {
+    private class LowerLeft implements Runnable {
     	
-    	int prob1;
-    	int prob2;
     	Answer answer;
     	int[] values;
     	
-    	public innerThread(int prob1, int prob2, Answer answer, int[] values) {
+    	int currentMin = Integer.MAX_VALUE;
+    	long startTime = System.currentTimeMillis();
+    	
+    	public LowerLeft(Answer answer, int[] values) {
     		
-    		this.prob1 = prob1;
-    		this.prob2 = prob2;
     		this.answer = answer;
     		this.values = values;
-    		
     	}
     	
     	public void run() {
+    		for (int i = 0; i < values.length / 2; i++) {
+    			for (int j = 0; j < i; j++) {
+    				if (Math.abs(values[i] - values[j]) < currentMin) {
+                		currentMin = Math.abs(values[i] - values[j]);
+                		
+                	}
+                        
+                        //cd++;
+        				//System.out.println(cd);
+                }
+        }
+    	System.out.println("LowerLeft took" + (System.currentTimeMillis() - startTime));
+        answer.setAnswer(currentMin);
+    	
+    }
+    }
+   
+    private class BottomRight implements Runnable {
+    	
+    	Answer answer;
+    	int[] values;
+    	int currentMin = Integer.MAX_VALUE;
+    	long startTime = System.currentTimeMillis();
+    	
+    	public BottomRight(Answer answer, int[] values) {
     		
-    		int i = prob1;
+    		this.answer = answer;
+    		this.values = values;
+    	}
+    	
+    	public void run() {
+    		for (int i = values.length/2; i < values.length; i++) {
+    			for (int j = 0; j+values.length/2 < i - 1; j++) {
+    				if (Math.abs(values[i] - values[j]) < currentMin) {
+                		currentMin = Math.abs(values[i] - values[j]);
+                		
+                	}
+                        
+                        //cd++;
+        				//System.out.println(cd);
+                }
     		
-    		for (int j = 0; j < i; j++) {
-    			int diff = Math.abs(values[i] - values[j]);
-    			answer.setAnswer(diff);
     		}
-    		
-    		i = prob2;
-    		
-    		for (int j = 0; j < i; j++) {
-    			int diff = Math.abs(values[i] - values[j]);
-    			answer.setAnswer(diff);
-    		}
-    		
+    		System.out.println("BottomRight took" + (System.currentTimeMillis() - startTime));
+        answer.setAnswer(currentMin);
     	}
     	
     }
-    
+
+    private class Middle implements Runnable {
+
+        Answer answer;
+        int[] values;
+        long startTime = System.currentTimeMillis();
+
+        int currentMin = Integer.MAX_VALUE;
+        
+        public Middle(Answer answer, int[] values) {
+
+                this.answer = answer;
+                this.values = values;
+        }
+
+        public void run() {
+                for (int i = values.length/2; i < values.length; i++) {
+                        for (int j = i-values.length/2; j < values.length/2; j++) {
+                        	if (Math.abs(values[i] - values[j]) < currentMin) {
+                        		currentMin = Math.abs(values[i] - values[j]);
+                        		
+                        	}
+                                
+                                //cd++;
+                				//System.out.println(cd);
+                        }
+                }
+                System.out.println("Middle took" + (System.currentTimeMillis() - startTime));
+                answer.setAnswer(currentMin);
+        }
+
+    }
+
+    private class TopRight implements Runnable {
+
+        Answer answer;
+        int[] values;
+
+        long startTime = System.currentTimeMillis();
+        
+        int currentMin = Integer.MAX_VALUE;
+        
+        public TopRight(Answer answer, int[] values) {
+
+                this.answer = answer;
+                this.values = values;
+        }
+
+        public void run() {
+                for (int i = values.length/2; i < values.length; i++) {
+                        for (int j = values.length/2; j < i; j++) {
+                        	if (Math.abs(values[i] - values[j]) < currentMin) {
+                        		currentMin = Math.abs(values[i] - values[j]);
+                        		
+                        	}
+                                
+                                //cd++;
+                				//System.out.println(cd);
+                        }
+                }
+                System.out.println("TopRight took" + (System.currentTimeMillis() - startTime));
+                answer.setAnswer(currentMin);
+        
+        }
+
+    }
+
+
     private class Answer {
     	
     	private int content;
